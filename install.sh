f4ab4c1d0689a7048030246d30b6e1750c0e29d2
edwardspencerdev <edwardspencerdev@outlook.com>
2025-08-20 20:01:16 +0100

    Added Mediamtx for WebRTC and RTSP

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
+OLDPWD=$PWD
 cp ./eth-wifi-switch.sh /usr/local/bin/eth-wifi-switch.sh
 chmod +x /usr/local/bin/eth-wifi-switch.sh
 cp ./eth-wifi-switch.service /etc/systemd/system/eth-wifi-switch.service
 systemctl enable eth-wifi-switch.service
+mkdir /usr/local/bin/mediamtx
+cd /usr/local/bin/mediamtx
+wget https://github.com/bluenviron/mediamtx/releases/download/v1.14.0/mediamtx_v1.14.0_linux_arm64.tar.gz
+tar xzf mediamtx_v1.14.0_linux_arm64.tar.gz
+cd $OLDPWD
+cp ./mediamtx.service /etc/systemd/system/mediamtx.service
+cp ./mediamtx.yml /usr/local/bin/mediamtx/
